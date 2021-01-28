24a18b54c0f1c662022e4c6acde5b47a8197a13b
Punn Siriphanthong <p.siriphanthong@gmail.com>
2021-01-28 22:11:46 +0700

    feat(api-gateway): handle error from json-server

--- api-gateway/controllers/trip.controller.js
+++ api-gateway/controllers/trip.controller.js
@@ -5,7 +5,7 @@ const Trip = require('../models/trip.model')
 const jsonServer = apiAdapter(jsonServerUrl)
 
 module.exports = {
-  getTrips: async (req, res) => {
+  getTrips: async (req, res, next) => {
     const keyword = req.query.keyword || ''
     const cursor = +req.query.cursor || 0
     const limit = +req.query.limit || 10
@@ -13,7 +13,7 @@ module.exports = {
     // "attr" param is not available in json-server for now
     // waiting for merge pull request https://github.com/typicode/json-server/pull/558
     const url = `/trips?q=${keyword}&attr=title,description,tags&_start=${cursor}&_limit=${limit}`
-    const response = await jsonServer.get(encodeURI(url))
+    const response = await jsonServer.get(encodeURI(url)).catch(next)
 
     const totalCount = +response.headers['x-total-count']
     const data = response.data.map(data => new Trip(data))
